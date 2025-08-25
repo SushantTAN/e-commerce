bda4652354bc823d9e23ab736e51d3eb419f0518
SushantTAN <punk1susant@gmail.com>
2025-08-25 18:06:35 +0545

    Removed dist files

--- backend/dist/models/product.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-class Product extends sequelize_1.Model {
-}
-// Note: The actual Product.init is done in backend/src/models/index.ts
-// This file primarily serves for type definitions and potentially associations if defined here.
-exports.default = Product;
